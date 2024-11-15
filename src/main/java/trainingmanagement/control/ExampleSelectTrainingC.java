40572aed5179a825982852fd51e4490cf27123af
Tim Zolleis <tim@zolleis.net>
2024-11-15 10:52:16 +0100

    refactor(select-training): refactor example code

---
+++ src/main/java/trainingmanagement/control/ExampleSelectTrainingC.java
@@ -0,0 +1,41 @@
+package trainingmanagement.control;
+
+import trainingmanagement.entity.Clerk;
+import trainingmanagement.entity.Training;
+
+import java.util.Collection;
+import java.util.Set;
+
+class ExampleSelectTrainingC {
+    static String[] getAllNames() {
+        return Training.getAllNames();
+    }
+
+    public final String[] getTrainingNamesToAttend(final String username) {
+        final Clerk clerk = Clerk.getClerk(username);
+        final Collection<Training> trainings = Training.getAllTrainings().values();
+        return trainings.stream().filter(training -> isToAttend(clerk, training)).map(Training::getName).toArray(String[]::new);
+
+    }
+
+    public final String[] getTrainingsNamesToComplete(final String username) {
+        final Clerk clerk = Clerk.getClerk(username);
+        final Collection<Training> trainings = Training.getAllTrainings().values();
+        return trainings.stream().filter(training -> hasCompletedTraining(clerk, training)).map(Training::getName).toArray(String[]::new);
+    }
+
+    private boolean isToAttend(final Clerk clerk, final Training training) {
+        return !clerk.hasAttendedTraining(training) && !clerk.hasCompletedTraining(training) && hasAllRequirements(clerk, training);
+    }
+
+    private boolean hasAllRequirements(final Clerk clerk, final Training training) {
+        final Set<Training> requirements = training.getRequirements();
+        return requirements.stream().allMatch(clerk::hasCompletedTraining);
+
+    }
+
+    private boolean hasCompletedTraining(final Clerk clerk, final Training training) {
+        return !clerk.hasAttendedTraining(training) && hasAllRequirements(clerk, training);
+    }
+
+}
